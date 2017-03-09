820a183425d0a19ee069d1c00871723aee99ed15
mvoss9000 <mvoss9000@yahoo.com>
2017-03-09 11:37:16 -0500

    update api to standard glossary

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-module.exports = function(buf, opt) {
-    opt = opt || {}
-    var i = opt.beg || 0
-    var end = opt.end == null ? buf.length : opt.end
+module.exports = function (src, off, lim) {
+    lim = lim == null ? src.length : lim
+    var i = off || 0
+    if (i >= lim) { return [] }
+
     var ret = []
-    if(i >= end) { return [] }
-    main: while(i < end) {
-        while(buf[i] < 0x80) { if(++i === end) break main }   // skip ascii
-        // non-ascii
+    while (i < lim) {
+        while (src[i] < 0x80) { if (++i === lim) return ret }       // skip ascii
+
+        // non-ascii (2 or more bytes)
         var start = i
-        var c = buf[i++]
-        if(c >= 0xC0) {
-            for(var n=2; (c << n) & 0x80; n++)   {}        // count sequential 1's: 11xxxxxx
-            while(i<end && (buf[i] & 0xC0) === 0x80) {i++}   // skip trailing bytes
-            if(i-start !== n) {
-                if(i-start > n) {
-                    ret.push([start + n, i])    // excess trailing bytes
+        var c = src[i++]
+        if (c >= 0xC0) {
+            // trailing bytes
+            for (var n = 2; (c << n) & 0x80; n++) {}                // count sequential 1's: 11xxxxxx
+            while (i < lim && (src[i] & 0xC0) === 0x80) { i++ }     // skip trailing bytes
+            if (i - start !== n) {
+                if (i - start > n) {
+                    ret.push([start + n, i])                        // excess trailing bytes
                 } else {
-                    ret.push([start, i])        // not enough trailing bytes
+                    ret.push([start, i])                            // not enough trailing bytes
                 }
             }
         } else {
             // unexpected trailing byte
-            while(i<end && (buf[i] & 0xC0) === 0x80) {i++}
-            ret.push([start, i])                // skip all trailing bytes
+            while (i < lim && (src[i] & 0xC0) === 0x80) { i++ }
+            ret.push([start, i])                                    // skip all trailing bytes
         }
     }
     return ret
--- test.js
+++ test.js
@@ -9,45 +9,45 @@ test('illegal_bytes options', function(t) {
         0x62,       // 'b'
     ]
     t.tableAssert([
-        [ 'buf',                                 'opt',               'exp'                   ],
+        [ 'buf',                                 'off',  'lim',               'exp'                   ],
 
         // all short
-        [ [0xF0, 0xE0, 0xC0 ],                    null,               [ [0,1], [1,2], [2,3] ] ],
-        [ [0xF0, 0x61, 0xE0, 0x62, 0xC0 ],        null,               [ [0,1], [2,3], [4,5] ] ],
-        [ [0xF0, 0x61, 0xE0, 0x62, 0xC0 ],        {beg:1},            [ [2,3], [4,5] ]        ],
-        [ [0xF0, 0x61, 0xE0, 0x62, 0xC0 ],        {beg:1,end:5},      [ [2,3], [4,5] ]        ],
-        [ [0xF0, 0x61, 0xE0, 0x62, 0xC0 ],        {beg:1,end:4},      [ [2,3] ]               ],
-        [ [0xF0, 0x61, 0xE0, 0x62, 0xC0 ],        {beg:1,end:3},      [ [2,3] ]               ],
-        [ [0xF0, 0x61, 0xE0, 0x62, 0xC0 ],        {beg:1,end:2},      []                      ],
+        [ [0xF0, 0xE0, 0xC0 ],                    null,   null,       [ [0,1], [1,2], [2,3] ] ],
+        [ [0xF0, 0x61, 0xE0, 0x62, 0xC0 ],        null,   null,       [ [0,1], [2,3], [4,5] ] ],
+        [ [0xF0, 0x61, 0xE0, 0x62, 0xC0 ],        1,      null,       [ [2,3], [4,5] ]        ],
+        [ [0xF0, 0x61, 0xE0, 0x62, 0xC0 ],        1,      5,          [ [2,3], [4,5] ]        ],
+        [ [0xF0, 0x61, 0xE0, 0x62, 0xC0 ],        1,      4,          [ [2,3] ]               ],
+        [ [0xF0, 0x61, 0xE0, 0x62, 0xC0 ],        1,      3,          [ [2,3] ]               ],
+        [ [0xF0, 0x61, 0xE0, 0x62, 0xC0 ],        1,      2,          []                      ],
 
         // no leads
-        [ [0xA7, 0xA8, 0xA9 ],                    null,               [ [0,3] ]               ],
-        [ [0xA7, 0xA8, 0xA9 ],                    {beg:1},            [ [1,3] ]               ],
-        [ [0xA7, 0xA8, 0xA9 ],                    {beg:2},            [ [2,3] ]               ],
-        [ [0xA7, 0xA8, 0xA9 ],                    {beg:3},            []                      ],
-        [ [0xA7, 0xA8, 0xA9 ],                    {end:2},            [ [0,2] ]               ],
-        [ [0xA7, 0x61, 0xA8, 0x62, 0xA9 ],        null,               [ [0,1], [2,3], [4,5] ] ],
-        [ [0x61, 0x62, 0x63, 0xA7, 0x61, 0x62 ],  null,               [ [3,4] ]               ],
+        [ [0xA7, 0xA8, 0xA9 ],                    null,   null,      [ [0,3] ]               ],
+        [ [0xA7, 0xA8, 0xA9 ],                    1,      null,      [ [1,3] ]               ],
+        [ [0xA7, 0xA8, 0xA9 ],                    2,      null,      [ [2,3] ]               ],
+        [ [0xA7, 0xA8, 0xA9 ],                    3,      null,      []                      ],
+        [ [0xA7, 0xA8, 0xA9 ],                    null,   2,         [ [0,2] ]               ],
+        [ [0xA7, 0x61, 0xA8, 0x62, 0xA9 ],        null,   null,      [ [0,1], [2,3], [4,5] ] ],
+        [ [0x61, 0x62, 0x63, 0xA7, 0x61, 0x62 ],  null,   null,      [ [3,4] ]               ],
 
         // range tests
-        [  buf,                                   null,               []                      ],
-        [  buf,                                   {beg:0},            []                      ],
-        [  buf,                                   {end:4},            []                      ],
-        [  buf,                                   {beg:0,end:0},      []                      ],
-        [  buf,                                   {beg:4,end:4},      []                      ],
-        [  buf,                                   {beg:0,end:4},      []                      ],
-        [  buf,                                   {beg:1,end:4},      []                      ],
-        [  buf,                                   {beg:2,end:4},      [[2,3]]                 ],
-        [  buf,                                   {beg:3,end:4},      []                      ],
-        [  buf,                                   {beg:4,end:4},      []                      ],
-        [  buf,                                   {beg:0,end:0},      []                      ],
-        [  buf,                                   {beg:0,end:1},      []                      ],
-        [  buf,                                   {beg:0,end:2},      [[1,2]]                 ],
-        [  buf,                                   {beg:0,end:3},      []                      ],
+        [  buf,                                   null,   null,       []                      ],
+        [  buf,                                   0,      null,       []                      ],
+        [  buf,                                   null,   4,          []                      ],
+        [  buf,                                   0,      0,          []                      ],
+        [  buf,                                   4,      4,          []                      ],
+        [  buf,                                   0,      4,          []                      ],
+        [  buf,                                   1,      4,          []                      ],
+        [  buf,                                   2,      4,          [[2,3]]                 ],
+        [  buf,                                   3,      4,          []                      ],
+        [  buf,                                   4,      4,          []                      ],
+        [  buf,                                   0,      0,          []                      ],
+        [  buf,                                   0,      1,          []                      ],
+        [  buf,                                   0,      2,          [[1,2]]                 ],
+        [  buf,                                   0,      3,          []                      ],
 
         // mixed tests
         //  ok     short      ok   short     ok      xlead xlead  ok   xlead
         // ----  ----------  ----  ----  ----------  ----  ----- ----  ----
-        [ [0x61, 0xF0, 0x90, 0x62, 0xC3, 0xC3, 0xA9, 0xA9, 0xA9, 0x63, 0xA9 ], null, [ [1,3], [4,5], [7,9], [10,11]] ],
+        [ [0x61, 0xF0, 0x90, 0x62, 0xC3, 0xC3, 0xA9, 0xA9, 0xA9, 0x63, 0xA9 ], null, null, [ [1,3], [4,5], [7,9], [10,11]] ],
     ], illegal_bytes)
 })
